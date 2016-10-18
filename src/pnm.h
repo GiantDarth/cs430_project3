19b1cee2f05201234ae9cd036731a799565b86f3
Christopher Robert Philabaum <christopher@philabaum.me>
2016-10-18 12:14:39 -0700

    Added clamp pixel function.

--- src/pnm.h
+++ src/pnm.h
@@ -25,4 +25,19 @@ typedef struct pixel {
     unsigned char blue;
 } pixel;
 
+static inline double clamp(double value, double min, double max) {
+    if(value < min) {
+        return min;
+    }
+    else if(value > max) {
+        return max;
+    }
+}
+
+static inline void clampPixel(pixel* pixel) {
+    pixel->red = clamp(pixel->red, 0, 1);
+    pixel->green = clamp(pixel->green, 0, 1);
+    pixel->blue = clamp(pixel->blue, 0, 1);
+}
+
 #endif // CS430_PNM_H
