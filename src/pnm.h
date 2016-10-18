a3b274ef70fc5539f1deb4519d691ef482144da2
Christopher Robert Philabaum <christopher@philabaum.me>
2016-10-18 12:45:19 -0700

    Added pixel to vector conversion.

--- src/pnm.h
+++ src/pnm.h
@@ -3,6 +3,8 @@
 
 #include <stddef.h>
 
+#include "vector3d.h"
+
 #define CS430_PNM_MIN 1
 #define CS430_PNM_BITMAP_MAX 255
 #define CS430_PNM_GREY_MAX 65535
@@ -34,10 +36,30 @@ static inline double clamp(double value, double min, double max) {
     }
 }
 
-static inline void clampPixel(pixel* pixel) {
+static inline void pixel_clamp(pixel* pixel) {
     pixel->red = clamp(pixel->red, 0, 1);
     pixel->green = clamp(pixel->green, 0, 1);
     pixel->blue = clamp(pixel->blue, 0, 1);
+}
+
+static inline vector3d pixel2Vector3d(pixel pixel) {
+    vector3d vector = {
+        pixel.red,
+        pixel.green,
+        pixel.blue
+    };
+
+    return vector;
+}
+
+static inline pixel vector3d2pixel(vector3d vector) {
+    pixel pixel = {
+        vector.x,
+        vector.y,
+        vector.z
+    };
+
+    return pixel;
 }
 
 #endif // CS430_PNM_H
